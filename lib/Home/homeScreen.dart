12c4995459e42e975125186e7258a57c9de7b768
mahesh <yernagulamahesh@gmail.com>
2023-04-24 18:43:57 +0530

    Listview class

--- lib/Home/homeScreen.dart
+++ lib/Home/homeScreen.dart
@@ -10,17 +10,97 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> names = ["shivani", "dinesh", "gowtham", "Lucid"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen")),
-      body: Center(
-          child: TextButton(
-        child: Text("Back"),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )),
-    );
+        appBar: AppBar(title: Text("Home Screen")),
+        body: ListView.separated(
+          // scrollDirection: Axis.horizontal,
+          separatorBuilder: (context, index) {
+            // return Divider(
+            //   height: 5,
+            //   color: Colors.red,
+            // );
+            return Container(
+              height: 10,
+              color: Colors.blue,
+            );
+          },
+          itemCount: names.length,
+          itemBuilder: (context, index) {
+            final data = names[index];
+            return Container(
+              // margin: EdgeInsets.all(10),
+              height: 100,
+              width: 200,
+              child: Text(data),
+              color: Colors.orange,
+            );
+          },
+        ));
   }
 }
+
+//ListView
+
+/*
+ListView(
+          children: [
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.orange,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.black,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.orange,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.orange,
+            ),  Container(
+              height: 100,
+              width: 200,
+              color: Colors.black,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.orange,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.orange,
+            )
+          ],
+        )
+ */
